c06b2852ae471bc5971f4fd1318a002c17142fc1
Connor Widder <Connor.Widd@gmail.com>
2020-03-08 12:17:52 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pairWar
+//
+//  Created by Connor Widder on 3/8/20.
+//  Copyright © 2020 Connor Widder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
